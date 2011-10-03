7c4505d944bf3f8d5b333301180225a92d8c85e0
ch33kybutt <ch33kybutt@yahoo.com>
2011-10-03 16:58:10 +0800

    Derp fix

--- products/oxygen_passion.mk
+++ products/oxygen_passion.mk
@@ -12,13 +12,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=passion BUILD_ID=GRK39F BUILD_DISPL
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.ril.enable.managed.roaming=1 \
     ro.ril.oem.nosim.ecclist=911,112,999,000,08,118,120,122,110,119,995 \
-    ro.ril.emc.mode=2 \
-    ro.ril.hep=1 \
-    ro.ril.hsxpa=2 \
-    ro.ril.gprsclass=12 \
-    ro.ril.hsdpa.category=8 \
-    ro.ril.hsupa.category=5 \
-    ro.ril.mtu1472=1
+    ro.ril.emc.mode=2
 
 PRODUCT_PACKAGES += \
     FM \
@@ -26,17 +20,19 @@ PRODUCT_PACKAGES += \
     Torch \
     DSPManager \
     libcyanogen-dsp \
-    libFLAC
+    libFLAC \
+    VoiceDialer
 
 PRODUCT_COPY_FILES += \
     vendor/oxygen/prebuilt/etc/resolv.conf:system/etc/resolv.conf \
+    vendor/oxygen/prebuilt/etc/sysctl.conf:system/etc/sysctl.conf \
     vendor/oxygen/prebuilt/etc/init.d/01sysctl:system/etc/init.d/01sysctl \
     vendor/oxygen/prebuilt/etc/init.d/04modules:system/etc/init.d/04modules \
     vendor/oxygen/prebuilt/etc/init.d/05mountsd:system/etc/init.d/05mountsd \
     vendor/oxygen/prebuilt/etc/init.d/06mountdl:system/etc/init.d/06mountdl \
     vendor/oxygen/prebuilt/etc/init.d/20userinit:system/etc/init.d/20userinit \
     vendor/oxygen/prebuilt/xbin/e2fsck:system/xbin/e2fsck \
-    vendor/oxygen/prebuilt/xbin/tune2fs:system/xbin/tune2fs    
+    vendor/oxygen/prebuilt/xbin/tune2fs:system/xbin/tune2fs   
 
 # Extra passion overlay
 PRODUCT_PACKAGE_OVERLAYS += vendor/oxygen/overlay/passion
